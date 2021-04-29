c6e564ce2ecc0146b1196c97ecafa2b86965ca0e
kosmar2011 <kosmar2011@gmail.com>
2021-04-29 10:48:05 +0300

    run

--- src/OLD-VidStab.cpp
+++ src/OLD-VidStab.cpp
@@ -7,33 +7,33 @@
 #include <opencv2/core/types_c.h>
 #include <opencv2/imgproc/imgproc.hpp>
 
-#include "VidStab.hpp"
+#include <fftw3.h>
 #include <chrono> 
 #include <vector> 
 
 
-// //const uchar channels = 3;
-// const uint16_t height = 480;
-// const uint16_t width = 640;
-// const uint32_t N = height * width;
+//const uchar channels = 3;
+const uint16_t height = 480;
+const uint16_t width = 640;
+const uint32_t N = height * width;
 
 
-// inline void cross_power_spectrum(fftw_complex* in1, fftw_complex* in2, fftw_complex* pc) {
-//     for (uint32_t i = 0; i < 2 * N - 1; i++) {
-//         //complex multiplication
-//         double a = in1[i][0];
-//         double c = in2[i][0]; // read from (and write to!) each non-local variable exactly once in every function.
-//         double b = in1[i][1];
-//         double d = in2[i][1];
-//         double m0 = a * c - b * d;
-//         double m1 = a * d + b * c;
-//         //absolute
-//         auto abs = std::sqrt(m0 * m0 + m1 * m1);
-//         //regularization
-//         pc[i][0] = m0 / abs;
-//         pc[i][1] = m1 / abs;
-//     }
-// }
+inline void cross_power_spectrum(fftw_complex* in1, fftw_complex* in2, fftw_complex* pc) {
+    for (uint32_t i = 0; i < 2 * N - 1; i++) {
+        //complex multiplication
+        double a = in1[i][0];
+        double c = in2[i][0]; // read from (and write to!) each non-local variable exactly once in every function.
+        double b = in1[i][1];
+        double d = in2[i][1];
+        double m0 = a * c - b * d;
+        double m1 = a * d + b * c;
+        //absolute
+        auto abs = std::sqrt(m0 * m0 + m1 * m1);
+        //regularization
+        pc[i][0] = m0 / abs;
+        pc[i][1] = m1 / abs;
+    }
+}
 
 inline void conj(fftw_complex* fft, fftw_complex* conj) {
     for (uint32_t i = 0; i < 2 * N - 1; i++) {
